13043a74a6570b81fd937e59c7c6e85d83e22329
Eathylik <louis.jcntpro@gmail.com>
2024-05-30 15:58:03 +0200

    exo JS

--- ToDoList/js/ToDoList.js
+++ ToDoList/js/ToDoList.js
@@ -2,22 +2,52 @@ let task = document.getElementById("task");
 let add = document.querySelector("#add");
 let list = document.getElementById("list");
 
-let array = [];
 
 
+add.addEventListener("click",ajouterTache);
 
-add.addEventListener("click",displayText);
+let array = JSON.parse(localStorage.getItem("data")) || [];
 
+// /!\ Attention les ligne sont lu de HAUT en BAS /!\
 
 function displayText (){
-    let li = document.createElement("li");
-    li.textContent = task.value;
-    list.appendChild(li);
-    array.push(task.value);
-    console.log(array);
-    localStorage.setItem("data", JSON.stringify(array));
-    task.value ="";
-    task.focus()
+    list.innerHTML = "";
+    for (let item of array) {
+        let li = document.createElement("li");
+        li.textContent = item;
+        list.appendChild(li);
+        let btn = document.createElement("button")
+        btn.textContent = "supprimer"
+        li.appendChild(btn)
+        list.appendChild(li)
+        btn.addEventListener("click", function(){
+            let indice = array.indexOf(item)
+            array.splice(indice,1)
+            localStorage.setItem("data", JSON.stringify(array))
+            displayText()
+        })
+    }
+    // array.push(task.value);
+    // localStorage.setItem("data", JSON.stringify(array));
+    // task.value = "";
+    // task.focus();
 }
+displayText()
 
+function ajouterTache(){
+    
+    if (array.find(el => el === task.value) || !task.value ){
+        // alert("la Valeur de " + task.value + " existe déjà");
+        alert(`la valeur de ${task.value} existe déjà`)
+        task.focus()
+        task.value = "";
+    } else {
+        console.log(task.value , "n'esxiste pas");
+        array.push(task.value)
+        localStorage.setItem("data", JSON.stringify(array));
+        task.value = "";
+        task.focus();
+        displayText()
+    }
+}
 
