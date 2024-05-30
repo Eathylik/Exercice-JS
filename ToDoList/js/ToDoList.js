1b3816d397006dd4aafc2fb5071a622d3a638db5
Eathylik <louis.jcntpro@gmail.com>
2024-05-30 10:35:37 +0200

    exo JS

--- ToDoList/js/ToDoList.js
+++ ToDoList/js/ToDoList.js
@@ -1,15 +1,23 @@
-let task = document.getElementById("task")
-let add = document.querySelector("#add")
-let list = document.getElementById("list")
+let task = document.getElementById("task");
+let add = document.querySelector("#add");
+let list = document.getElementById("list");
+
+let array = [];
 
 
 
-
-add.addEventListener("click",displayText) 
+add.addEventListener("click",displayText);
 
 
 function displayText (){
-    console.log(task.value);
+    let li = document.createElement("li");
+    li.textContent = task.value;
+    list.appendChild(li);
+    array.push(task.value);
+    console.log(array);
+    localStorage.setItem("data", JSON.stringify(array));
+    task.value ="";
+    task.focus()
 }
 
 
