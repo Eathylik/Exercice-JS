2ecabbe5bca384b48fd46989e78e2cfecf28d3a0
Eathylik <louis.jcntpro@gmail.com>
2024-05-31 14:22:13 +0200

    exo JS

--- calculatrice/js/index.js
+++ calculatrice/js/index.js
@@ -1,8 +1,28 @@
-const input = document.querySelector("#input")
+const paragraphe = document.querySelector("#paragraphe")
 const clear = document.querySelector("#éffacé")
 const egal = document.querySelector("#egal")
 const btn = document.querySelectorAll("button")
 
-function add (value){
-    input.textContent += value
+function add (value) {
+    paragraphe.textContent += value
+    console.log(paragraphe.textContent);
 }
+
+function sup () {
+    paragraphe.textContent = ""
+}
+
+function calculate () {
+    try {
+        paragraphe.textContent = eval(paragraphe.textContent) 
+        console.log(eval(paragraphe.textContent));
+    } catch(e) {
+        paragraphe.textContent = e
+    }
+}
+
+document.addEventListener("keydown", (event) => {
+    if (document.isComposing || event === 48) {
+      return;
+    }
+  });
