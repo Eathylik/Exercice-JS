ac027abdfc6cfa865b5d73d21c30dd8af6e2c8f4
Eathylik <louis.jcntpro@gmail.com>
2024-05-31 11:56:59 +0200

    exo JS

---
+++ calculatrice/js/index.js
@@ -0,0 +1,8 @@
+const input = document.querySelector("#input")
+const clear = document.querySelector("#éffacé")
+const egal = document.querySelector("#egal")
+const btn = document.querySelectorAll("button")
+
+function add (value){
+    input.textContent += value
+}
