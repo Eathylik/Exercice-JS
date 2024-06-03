e6b5bf52982e1bfa50caa76d1909b71b35bb1488
Eathylik <louis.jcntpro@gmail.com>
2024-06-03 09:39:34 +0200

    exo JS

---
+++ Les Modules/js/add.js
@@ -0,0 +1,6 @@
+//Crée une fonction qui retourne l'addition de nombre
+
+
+export default function calculate (a, b) {
+    return a + b
+}
---
+++ Les Modules/js/main.js
@@ -0,0 +1,3 @@
+import calculate from "./add.js"
+
+console.log(calculate(1,5));
