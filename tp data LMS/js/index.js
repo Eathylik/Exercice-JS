3969fd57e6661e0eab1a38933fade89777e80d5e
Eathylik <louis.jcntpro@gmail.com>
2024-06-07 09:53:01 +0200

    exo JS

--- tp data LMS/js/index.js
+++ tp data LMS/js/index.js
@@ -1,14 +1,3 @@
-// import user from "./module/getDatas.js";
-
-// import createTbody from "./module/createTbody.js";
-
-// const base = await user();
-
-// createTbody(base);
-
 import getData from "./module/getTab.js";
 
-
-
 getData()
-
