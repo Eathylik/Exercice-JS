f2a3a27360d101a9516955a2c038c5b0df3cf594
Eathylik <louis.jcntpro@gmail.com>
2024-06-12 16:27:01 +0200

    exercice-JS

--- RAPPEL JS/js/main.js
+++ RAPPEL JS/js/main.js
@@ -1,4 +1,3 @@
 import { plus, moins } from "./module/compteur.js";
 import { getData } from "./module/fetchData.js";
 import { empty, submit } from "./module/nameForm.js";
-getData()
--- RAPPEL JS/js/module/fetchData.js
+++ RAPPEL JS/js/module/fetchData.js
@@ -1,5 +1,26 @@
-export async function getData (){
-    const reponse = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const data = await reponse.json ()
-    console.log(data);
+function createElement (item){
+    let article = document.createElement ('article')
+    let h3 = document.createElement ('h3')
+    let p = document.createElement ('p')
+    let link = document.createElement('a')
+    h3.textContent = item.title
+    p.textContent = item.body
+    link.textContent = "plus..."
+    link.setAttribute('href', `https://jsonplaceholder.typicode.com/posts/${item.id}`)
+    article.appendChild(h3)
+    article.appendChild(p)
+    article.appendChild(link)
+    // document.getElementById('container').appendChild(article)
+    return article
 }
+
+export async function getData (){
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+    data.map (item => {
+        
+        document.getElementById('container').appendChild(createElement(item))
+    })
+}
+
+getData()
